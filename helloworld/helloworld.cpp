b447dbe6c8c13fddaa43b63368624e0976898b66
bjhwchao <haowenchao123@gmail.com>
2020-03-11 10:54:41 +0800

    helloworld

--- helloworld/helloworld.cpp
+++ helloworld/helloworld.cpp
@@ -1,8 +1,22 @@
 #include <iostream> 
 
+class Test
+{
+public:
+    int add(int a, int b);
+
+};
+
+int Test::add(int a, int b)
+{
+    return a + b; 
+}
+
 int main()
 {
     std::cout << "Hello, World!" << std::endl; 
 
+    Test t = Test();
+    std::cout << t.add(1, 2) << std::endl; 
     return 0;
 }
